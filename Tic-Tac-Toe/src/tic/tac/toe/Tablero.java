004afd4ddf4cbdf0402e4c56e7571c7cee93a2d1
estudiantes <estudiantes@labing00>
2017-03-10 17:24:49 -0500

    creada interfaz grafica

---
+++ Tic-Tac-Toe/src/tic/tac/toe/Tablero.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic.tac.toe;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Tablero {
+    
+    public char[][] ficha= new char[3][3];
+    
+    public void calculartriqui(){
+        
+    }
+    
+}
